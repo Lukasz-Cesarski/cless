2dbe248f1bc90654c8c5e3cfdcea1b5b519b8fa7
Lukasz Cesarski <l.cesarski@edrone.me>
2023-08-27 15:59:13 +0200

    LGBM first version

--- cless/cless.py
+++ cless/cless.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 
 import numpy as np
 import pandas as pd
@@ -10,35 +10,44 @@ import wandb
 from datasets import Dataset
 from transformers import (AutoConfig, AutoModelForSequenceClassification,
                           AutoTokenizer, DataCollatorWithPadding,
-                          EarlyStoppingCallback, HfArgumentParser, Trainer,
+                          EarlyStoppingCallback, Trainer,
                           TrainingArguments, set_seed)
 
-DATA_HOME_ENV = "DATA_HOME_DIR"
-DATA_HOME_DIR_DEFAULT = "/kaggle/input"
-DATA_TMP_ENV = "TMP_DIR"
-COMPETITION_SUBDIR = "commonlit-evaluate-student-summaries"
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+import re
+from autocorrect import Speller
+from spellchecker import SpellChecker
+
+
 PRO_TRAIN_FILE = "prompts_train.csv"
 PRO_TEST_FILE = "prompts_test.csv"
 SUM_TRAIN_FILE = "summaries_train.csv"
 SUM_TEST_FILE = "summaries_test.csv"
 
+WANDB_FOLDS_PROJECT = "cless-folds"
+WANDB_ENSAMBLE_PROJECT = "cless-ensamble"
+
+ID2FOLD = {
+    "814d6b": 0,
+    "39c16e": 1,
+    "3b9047": 2,
+    "ebad26": 3,
+}
 
 def read_data(
-    input_data_dir: Optional[str] = None,
+        input_data_dir: str,
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
 
-    if input_data_dir is None:
-        input_data_dir = os.environ.get(DATA_HOME_ENV)
-    if input_data_dir is None:
-        input_data_dir = DATA_HOME_DIR_DEFAULT
     data_dir = Path(input_data_dir)
     if not data_dir.exists():
         raise FileNotFoundError(f"Directory not found! {data_dir}")
 
-    train_pro = pd.read_csv(data_dir / COMPETITION_SUBDIR / PRO_TRAIN_FILE)
-    test_pro = pd.read_csv(data_dir / COMPETITION_SUBDIR / PRO_TEST_FILE)
-    train_sum = pd.read_csv(data_dir / COMPETITION_SUBDIR / SUM_TRAIN_FILE)
-    test_sum = pd.read_csv(data_dir / COMPETITION_SUBDIR / SUM_TEST_FILE)
+    train_pro = pd.read_csv(data_dir / PRO_TRAIN_FILE)
+    test_pro = pd.read_csv(data_dir / PRO_TEST_FILE)
+    train_sum = pd.read_csv(data_dir / SUM_TRAIN_FILE)
+    test_sum = pd.read_csv(data_dir / SUM_TEST_FILE)
 
     return train_pro, test_pro, train_sum, test_sum
 
@@ -49,40 +58,57 @@ class Config:
         default="microsoft/deberta-v3-base",
         metadata={"help": "Model name or path"},
     )
-
     data_dir: Optional[str] = field(
         default="/kaggle/input/commonlit-evaluate-student-summaries",
         metadata={"help": "Data directory"},
     )
-
     max_seq_length: Optional[int] = field(
         default=512,
         metadata={"help": "Max sequence length"},
     )
-
     add_prompt_question: Optional[bool] = field(
         default=False,
         metadata={"help": "Add prompt question into input"},
     )
-
     add_prompt_text: Optional[bool] = field(
         default=False,
         metadata={"help": "Add prompt text into input"},
     )
-
-    fold: Optional[int] = field(
-        default=0,
-        metadata={"help": "Fold"},
-    )
-
     num_proc: Optional[int] = field(
         default=4,
-        metadata={"help": "Number of processes"},
+        metadata={"help": "Number of processes to tokenize dataset"},
     )
-
-    dropout: Optional[float] = field(
+    hidden_dropout_prob: Optional[float] = field(
         default=0.1,
-        metadata={"help": "Amount of dropout to apply"},
+        metadata={"help": "Amount of dropout to apply (hidden)"},
+    )
+    attention_probs_dropout_prob: Optional[float] = field(
+        default=0.1,
+        metadata={"help": "Amount of dropout to apply (attention)"},
+    )
+    learning_rate: Optional[float] = field(
+        default=5e-05,
+        metadata={"help": "Learning rate"},
+    )
+    weight_decay: Optional[float] = field(
+        default=0.0,
+        metadata={"help": "Weight decay"},
+    )
+    batch_size: Optional[int] = field(
+        default=8,
+        metadata={"help": "Size of the batch"},
+    )
+    num_train_epochs: Optional[int] = field(
+        default=3,
+        metadata={"help": "Number of training epochs"},
+    )
+    seed: Optional[int] = field(
+        default=42,
+        metadata={"help": "Random seed"},
+    )
+    report_to: Optional[str] = field(
+        default="wandb",
+        metadata={"help": "Where to log results"},
     )
 
 
@@ -131,126 +157,306 @@ def compute_mcrmse(eval_pred):
     }
 
 
-def training_loop(args=None):
-    if args is None:
-        args = ["--output_dir", "tmp"]
+class ClessDeberta:
+    def __init__(
+            self,
+            model_name_or_path: str,
+            tmp_dir: str,
+            dump_dir: str,
+    ):
+        self.model_name_or_path = model_name_or_path
+        self.tmp_dir = tmp_dir
+        self.dump_dir = dump_dir
 
-    parser = HfArgumentParser((Config, TrainingArguments))
-    config, training_args = parser.parse_args_into_dataclasses(args)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_or_path)
+        self.model_config = AutoConfig.from_pretrained(self.model_name_or_path)
 
-    set_seed(training_args.seed)
+        self.model_config.update(
+            {
+                "num_labels": 2,
+                "problem_type": "regression",
+            }
+        )
+        self.data_collator = DataCollatorWithPadding(
+            tokenizer=self.tokenizer
+        )
 
-    if "wandb" in training_args.report_to:
-        try:
-            from kaggle_secrets import UserSecretsClient
+    @staticmethod
+    def tokenize(
+            example,
+            tokenizer,
+            config,
+            labelled=True,
+    ):
+        sep = tokenizer.sep_token
 
-            user_secrets = UserSecretsClient()
-            key = user_secrets.get_secret("wandb_api")
-            wandb.login(key=key)
-        except:
-            print("Could not log in to WandB")
+        cols = []
 
-    run = wandb.init(reinit=True)
-    tokenizer = AutoTokenizer.from_pretrained(config.model_name_or_path)
-    model_config = AutoConfig.from_pretrained(config.model_name_or_path)
+        if config.add_prompt_question:
+            cols.append("prompt_question")
+        elif config.add_prompt_text:
+            cols.append("prompt_text")
+        cols.append("text")
 
-    model_config.update(
-        {
-            "hidden_dropout_prob": config.dropout,
-            "attention_probs_dropout_prob": config.dropout,
-            "num_labels": 2,
-            "problem_type": "regression",
+        if labelled:
+            labels = [example["content"], example["wording"]]
+
+        tokenized = tokenizer(
+            sep.join([example[c] for c in cols]),
+            padding=False,
+            truncation=True,
+            max_length=config.max_seq_length,
+        )
+
+        result = {**tokenized}  # inplace copy
+        if labelled:
+            result["labels"] = labels
+
+        return result
+
+    def train(
+            self,
+            config: Config,
+            fold: int,
+    ):
+        run = wandb.init(
+            reinit=True,
+            project=WANDB_FOLDS_PROJECT,
+            config=config.__dict__,
+        )
+        set_seed(config.seed)
+        self.model_config.update({
+            "hidden_dropout_prob": config.hidden_dropout_prob,
+            "attention_probs_dropout_prob": config.attention_probs_dropout_prob,
             "cfg": config.__dict__,
-        }
+        })
+
+        train_pro, test_pro, train_sum, test_sum = read_data(config.data_dir)
+        df = train_pro.merge(train_sum, on="prompt_id")
+        assert set(ID2FOLD.keys()) == set(df["prompt_id"].unique())
+        df["fold"] = df["prompt_id"].map(ID2FOLD)
+        train_ds = Dataset.from_pandas(df[df["fold"] != fold])
+        val_ds = Dataset.from_pandas(df[df["fold"] == fold])
+
+        train_ds = train_ds.map(
+            self.tokenize,
+            batched=False,
+            num_proc=config.num_proc,
+            fn_kwargs={"tokenizer": self.tokenizer, "config": config},
     )
+        val_ds = val_ds.map(
+            self.tokenize,
+            batched=False,
+            num_proc=config.num_proc,
+            fn_kwargs={"tokenizer": self.tokenizer, "config": config},
+        )
 
-    model = AutoModelForSequenceClassification.from_pretrained(
-        config.model_name_or_path, config=model_config, ignore_mismatched_sizes=True
-    )
+        if os.environ.get("CLESS_DRY_RUN") == "1":
+            dry_run_ds = train_ds.train_test_split(None, 20)
+            train_ds = dry_run_ds["train"]
 
-    training_args.greater_is_better = False
-    training_args.load_best_model_at_end = True
-    training_args.metric_for_best_model = "mcrmse"
-    # When load_best_model_at_end is set to True, the parameters save_strategy needs to be the same as evaluation_strategy,
-    # and in the case it is “steps”, save_steps must be a round multiple of eval_steps.
-    STEPS_VAL = 100
-    training_args.evaluation_strategy = "steps"
-    training_args.eval_steps = STEPS_VAL
-    training_args.save_total_limit = 2
-    training_args.num_train_epochs = 3
-    training_args.save_strategy = "steps"
-    training_args.save_steps = STEPS_VAL
+            dry_run_ds = val_ds.train_test_split(None, 10)
+            val_ds = dry_run_ds["train"]
 
-    # read data
-    train_pro, test_pro, train_sum, test_sum = read_data()
-    df = train_pro.merge(train_sum, on="prompt_id")
-    # 4 prompt ids, 4 folds
-    id2fold = {
-        "814d6b": 0,
-        "39c16e": 1,
-        "3b9047": 2,
-        "ebad26": 3,
-    }
-    assert set(id2fold.keys()) == set(df["prompt_id"].unique())
-    df["fold"] = df["prompt_id"].map(id2fold)
+        STEPS_VAL = 100
+        training_args = TrainingArguments(
+            output_dir=self.tmp_dir,
+            load_best_model_at_end=True,  # select best model
+            learning_rate=config.learning_rate,
+            per_device_train_batch_size=config.batch_size,
+            per_device_eval_batch_size=config.batch_size,
+            num_train_epochs=config.num_train_epochs,
+            weight_decay=config.weight_decay,
+            report_to=config.report_to,
+            greater_is_better=False,
+            save_strategy="steps",
+            evaluation_strategy="steps",
+            eval_steps=STEPS_VAL,
+            save_steps=STEPS_VAL,
+            metric_for_best_model="mcrmse",
+            save_total_limit=1
+        )
 
-    train_ds = Dataset.from_pandas(df[df["fold"] != config.fold])
-    val_ds = Dataset.from_pandas(df[df["fold"] == config.fold])
+        model = AutoModelForSequenceClassification.from_pretrained(
+            self.model_name_or_path,
+            config=self.model_config
+        )
 
-    train_ds = train_ds.map(
-        tokenize,
-        batched=False,
-        num_proc=config.num_proc,
-        fn_kwargs={"tokenizer": tokenizer, "config": config},
-    )
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_ds,
+            eval_dataset=val_ds,
+            data_collator=self.data_collator,
+            tokenizer=self.tokenizer,
+            compute_metrics=compute_mcrmse,
+            callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
+        )
+        trainer.train()
+        eval_res = trainer.evaluate()
 
-    val_ds = val_ds.map(
-        tokenize,
-        batched=False,
-        num_proc=config.num_proc,
-        fn_kwargs={"tokenizer": tokenizer, "config": config},
-    )
+        model_fold_dir = os.path.join(
+            self.dump_dir,
+            self.model_name_or_path.replace("/", "_"),
+            str(fold),
+        )
+        os.makedirs(model_fold_dir, exist_ok=True)
+        trainer.save_model(output_dir=model_fold_dir)
+        run.finish()
+        shutil.rmtree(self.tmp_dir)
 
-    if os.environ.get("CLESS_DRY_RUN") == "1":
-        dry_run_ds = train_ds.train_test_split(None, 30)
-        train_ds = dry_run_ds["train"]
+        return eval_res
 
-        dry_run_ds = val_ds.train_test_split(None, 10)
-        val_ds = dry_run_ds["train"]
+    def predict(self, df):
+        config = Config(**self.model_config.cfg)
+        # train_pro, test_pro, train_sum, test_sum = read_data(config.data_dir)
 
-    # print("Training set number of rows:", train_ds.num_rows)
-    # print("Validation set number of rows:", val_ds.num_rows)
+        # df = train_pro.merge(train_sum, on="prompt_id")
+        # df = test_pro.merge(test_sum, on="prompt_id")
+        test_ds = Dataset.from_pandas(df)
+        test_ds = test_ds.map(
+            tokenize,
+            batched=False,
+            num_proc=config.num_proc,
+            fn_kwargs={"tokenizer": self.tokenizer, "config": config, "labelled": False},
+        )
 
-    data_collator = DataCollatorWithPadding(
-        tokenizer=tokenizer,
-        pad_to_multiple_of=16 if training_args.fp16 else None,
-    )
+        model = AutoModelForSequenceClassification.from_pretrained(
+            self.model_name_or_path, config=self.model_config, ignore_mismatched_sizes=True
+        )
+        model.eval()
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_ds,
-        eval_dataset=val_ds,
-        data_collator=data_collator,
-        tokenizer=tokenizer,
-        compute_metrics=compute_mcrmse,
-        callbacks=[EarlyStoppingCallback(early_stopping_patience=3)],
-    )
+        test_args = TrainingArguments(
+            output_dir=self.tmp_dir,
+            do_train=False,
+            do_predict=True,
+            per_device_eval_batch_size=4,
+            dataloader_drop_last=False,
+        )
+        infer = Trainer(
+            model=model,
+            tokenizer=self.tokenizer,
+            data_collator=self.data_collator,
+            args=test_args)
+        predictions = infer.predict(test_dataset=test_ds)
 
-    trainer.train()
-    eval_res = trainer.evaluate()
+        return predictions
 
-    model.config.best_metric = trainer.state.best_metric
-    model.config.save_pretrained(training_args.output_dir)
 
-    dump_dir = os.path.join(
-        "model_dumps",
-        model_config.name_or_path.replace("/", "-") + f"_fold-{config.fold}",
-    )
-    os.makedirs(dump_dir, exist_ok=True)
-    trainer.save_model(output_dir=dump_dir)
-    shutil.rmtree(training_args.output_dir)
-    run.finish()
+class Preprocessor:
+    def __init__(self) -> None:
+        self.twd = TreebankWordDetokenizer()
+        self.STOP_WORDS = set(stopwords.words('english'))
 
-    return eval_res
+        self.speller = Speller(lang='en')
+        self.spellchecker = SpellChecker()
 
+    def word_overlap_count(self, row):
+        """ intersection(prompt_text, text) """
+
+        def check_is_stop_word(word):
+            return word in self.STOP_WORDS
+
+        prompt_words = row['prompt_tokens']
+        summary_words = row['summary_tokens']
+        if self.STOP_WORDS:
+            prompt_words = list(filter(check_is_stop_word, prompt_words))
+            summary_words = list(filter(check_is_stop_word, summary_words))
+        return len(set(prompt_words).intersection(set(summary_words)))
+
+    def ngrams(self, token, n):
+        # Use the zip function to help us generate n-grams
+        # Concatentate the tokens into ngrams and return
+        ngrams = zip(*[token[i:] for i in range(n)])
+        return [" ".join(ngram) for ngram in ngrams]
+
+    def ngram_co_occurrence(self, row, n: int) -> int:
+        # Tokenize the original text and summary into words
+        original_tokens = row['prompt_tokens']
+        summary_tokens = row['summary_tokens']
+
+        # Generate n-grams for the original text and summary
+        original_ngrams = set(self.ngrams(original_tokens, n))
+        summary_ngrams = set(self.ngrams(summary_tokens, n))
+
+        # Calculate the number of common n-grams
+        common_ngrams = original_ngrams.intersection(summary_ngrams)
+        return len(common_ngrams)
+
+    def quotes_count(self, row):
+        summary = row['text']
+        text = row['prompt_text']
+        quotes_from_summary = re.findall(r'"([^"]*)"', summary)
+        if len(quotes_from_summary) > 0:
+            return [quote in text for quote in quotes_from_summary].count(True)
+        else:
+            return 0
+
+    def spelling(self, text):
+
+        wordlist = text.split()
+        amount_miss = len(list(self.spellchecker.unknown(wordlist)))
+
+        return amount_miss
+
+    def add_spelling_dictionary(self, tokens: List[str]) -> List[str]:
+        """dictionary update for pyspell checker and autocorrect"""
+        self.spellchecker.word_frequency.load_words(tokens)
+        self.speller.nlp_data.update({token: 1000 for token in tokens})
+
+    def run(self,
+            prompts: pd.DataFrame,
+            summaries: pd.DataFrame,
+            ) -> pd.DataFrame:
+
+        # before merge preprocess
+        prompts["prompt_length"] = prompts["prompt_text"].apply(
+            lambda x: len(word_tokenize(x))
+        )
+        prompts["prompt_tokens"] = prompts["prompt_text"].apply(
+            lambda x: word_tokenize(x)
+        )
+
+        summaries["summary_length"] = summaries["text"].apply(
+            lambda x: len(word_tokenize(x))
+        )
+        summaries["summary_tokens"] = summaries["text"].apply(
+            lambda x: word_tokenize(x)
+        )
+
+        # Add prompt tokens into spelling checker dictionary
+        prompts["prompt_tokens"].apply(
+            lambda x: self.add_spelling_dictionary(x)
+        )
+
+        #         from IPython.core.debugger import Pdb; Pdb().set_trace()
+        # fix misspelling
+        summaries["fixed_summary_text"] = summaries["text"].progress_apply(
+            lambda x: self.speller(x)
+        )
+
+        # count misspelling
+        summaries["splling_err_num"] = summaries["text"].progress_apply(self.spelling)
+
+        # merge prompts and summaries
+        input_df = summaries.merge(prompts, how="left", on="prompt_id")
+
+        # after merge preprocess
+        input_df['length_ratio'] = input_df['summary_length'] / input_df['prompt_length']
+
+        input_df['word_overlap_count'] = input_df.progress_apply(self.word_overlap_count, axis=1)
+        input_df['bigram_overlap_count'] = input_df.progress_apply(
+            self.ngram_co_occurrence, args=(2,), axis=1
+        )
+        input_df['bigram_overlap_ratio'] = input_df['bigram_overlap_count'] / (input_df['summary_length'] - 1)
+
+        input_df['trigram_overlap_count'] = input_df.progress_apply(
+            self.ngram_co_occurrence, args=(3,), axis=1
+        )
+        input_df['trigram_overlap_ratio'] = input_df['trigram_overlap_count'] / (input_df['summary_length'] - 2)
+
+        input_df['quotes_count'] = input_df.progress_apply(self.quotes_count, axis=1)
+
+        input_df["fold"] = input_df["prompt_id"].map(ID2FOLD)
+
+        return input_df.drop(columns=["summary_tokens", "prompt_tokens"])
