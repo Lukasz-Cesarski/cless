64c252bbb497155163e13167d3a266c93b7aa6e1
Lukasz Cesarski <l.cesarski@edrone.me>
2023-09-17 18:34:30 +0200

    Add cublas to CLI

--- cless/model_training.py
+++ cless/model_training.py
@@ -10,31 +10,15 @@ import numpy as np
 import pandas as pd
 import torch
 import wandb
-from cless import (
-    ID2FOLD,
-    MODEL_DUMPS_DIR,
-    TARGET_LABELS,
-    ClessModel,
-    Config,
-    WandbProjects,
-    cless_model_ensamble_predict,
-    cless_model_ensamble_train,
-    cless_model_fold_predict,
-    compute_mcrmse_for_trainer,
-    read_cless_data,
-)
+from cless import (ID2FOLD, MODEL_DUMPS_DIR, TARGET_LABELS, ClessModel, Config,
+                   WandbProjects, cless_model_ensamble_predict,
+                   cless_model_ensamble_train, cless_model_fold_predict,
+                   compute_mcrmse_for_trainer, read_cless_data)
 from datasets import Dataset, disable_progress_bar
-from transformers import (
-    AutoConfig,
-    AutoModelForSequenceClassification,
-    AutoTokenizer,
-    DataCollatorWithPadding,
-    EarlyStoppingCallback,
-    HfArgumentParser,
-    Trainer,
-    TrainingArguments,
-    set_seed,
-)
+from transformers import (AutoConfig, AutoModelForSequenceClassification,
+                          AutoTokenizer, DataCollatorWithPadding,
+                          EarlyStoppingCallback, HfArgumentParser, Trainer,
+                          TrainingArguments, set_seed)
 
 
 @dataclass
@@ -43,23 +27,39 @@ class CommandLine:
         default="bxu2kp2l",
         metadata={"help": "Wandb Sweep run ID"},
     )
-
+    free_cublas: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Do not use determinism"},
+    )
 
 if __name__ == "__main__":
-    # deberta processing long texts is very sensitive for randomness
-    # https://pytorch.org/docs/stable/notes/randomness#avoiding-nondeterministic-algorithms
-    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-    torch.use_deterministic_algorithms(True)
-
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
     parser = HfArgumentParser((CommandLine,))
     (cl,) = parser.parse_args_into_dataclasses()
 
-    warnings.simplefilter("ignore")
+    if not cl.free_cublas:
+        # deberta processing long texts is very sensitive for randomness
+        # https://pytorch.org/docs/stable/notes/randomness#avoiding-nondeterministic-algorithms
+        os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+        torch.use_deterministic_algorithms(True)
+
     try:
-        wandb.login()
+        if any(k.startswith("KAGGLE") for k in os.environ.keys()):
+            # in kaggle environment import key from secrets
+            from kaggle_secrets import UserSecretsClient
+
+            user_secrets = UserSecretsClient()
+            key = user_secrets.get_secret("wandb_api")
+            wandb.login(key=key)
+        else:
+            # locally use env variable to pass key "export WANDB_API_KEY=...."
+            wandb.login()
     except:
         print("Could not log in to WandB")
         exit(1)
+
+    warnings.simplefilter("ignore")
+    start = datetime.now()
     run_id = cl.run_id
     api = wandb.Api()
     run = api.run(f"{WandbProjects.WANDB_DEBERTA_SWEEPS}/{run_id}")
@@ -74,3 +74,5 @@ if __name__ == "__main__":
     )
     pprint(config)
     fold_results = cless_model_ensamble_train(config=config)
+    print(fold_results)
+    print(f"Script timer: {datetime.now() - start}")
