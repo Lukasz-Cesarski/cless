fc69c637e25ec5d14c1c730441c0300850a5ae29
Lukasz Cesarski <l.cesarski@edrone.me>
2023-09-19 20:24:01 +0200

    Preprocessor turned on

--- cless/cless.py
+++ cless/cless.py
@@ -78,7 +78,7 @@ def get_wandb_tags():
     return tags
 
 
-def general_setup(free_cublas=False):
+def general_setup(free_cublas=False, internet_connection=True):
     warnings.simplefilter("ignore")
     os.environ["TOKENIZERS_PARALLELISM"] = "false"
     tqdm.pandas()  # for processor
@@ -94,15 +94,17 @@ def general_setup(free_cublas=False):
 
     try:
         if any(k.startswith("KAGGLE") for k in os.environ.keys()):
-            # in kaggle environment import key from secrets
-            from kaggle_secrets import UserSecretsClient
+            if internet_connection:
+                # in kaggle environment import key from secrets
+                from kaggle_secrets import UserSecretsClient
 
-            user_secrets = UserSecretsClient()
-            key = user_secrets.get_secret("wandb_api")
-            wandb.login(key=key)
+                user_secrets = UserSecretsClient()
+                key = user_secrets.get_secret("wandb_api")
+                wandb.login(key=key)
         else:
-            # locally use env variable to pass key "export WANDB_API_KEY=...."
-            wandb.login()
+            if internet_connection:
+                # locally use env variable to pass key "export WANDB_API_KEY=...."
+                wandb.login()
             # locally we need to specify data location
             os.environ[CLESS_DATA_ENV] = "input/"
     except:
@@ -470,255 +472,233 @@ def cless_ensamble_predict_test(models_home):
     return submission_df
 
 
-# class Preprocessor:
-#     def __init__(self) -> None:
-#         self.twd = TreebankWordDetokenizer()
-#         self.STOP_WORDS = set(stopwords.words("english"))
-#
-#         self.speller = Speller(lang="en")
-#         self.spellchecker = SpellChecker()
-#
-#     def word_overlap_count(self, row):
-#         """intersection(prompt_text, text)"""
-#
-#         def check_is_stop_word(word):
-#             return word in self.STOP_WORDS
-#
-#         prompt_words = row["prompt_tokens"]
-#         summary_words = row["summary_tokens"]
-#         if self.STOP_WORDS:
-#             prompt_words = list(filter(check_is_stop_word, prompt_words))
-#             summary_words = list(filter(check_is_stop_word, summary_words))
-#         return len(set(prompt_words).intersection(set(summary_words)))
-#
-#     def ngrams(self, token, n):
-#         # Use the zip function to help us generate n-grams
-#         # Concatentate the tokens into ngrams and return
-#         ngrams = zip(*[token[i:] for i in range(n)])
-#         return [" ".join(ngram) for ngram in ngrams]
-#
-#     def ngram_co_occurrence(self, row, n: int) -> int:
-#         # Tokenize the original text and summary into words
-#         original_tokens = row["prompt_tokens"]
-#         summary_tokens = row["summary_tokens"]
-#
-#         # Generate n-grams for the original text and summary
-#         original_ngrams = set(self.ngrams(original_tokens, n))
-#         summary_ngrams = set(self.ngrams(summary_tokens, n))
-#
-#         # Calculate the number of common n-grams
-#         common_ngrams = original_ngrams.intersection(summary_ngrams)
-#         return len(common_ngrams)
-#
-#     def quotes_count(self, row):
-#         summary = row["text"]
-#         text = row["prompt_text"]
-#         quotes_from_summary = re.findall(r'"([^"]*)"', summary)
-#         if len(quotes_from_summary) > 0:
-#             return [quote in text for quote in quotes_from_summary].count(True)
-#         else:
-#             return 0
-#
-#     def spelling(self, text):
-#
-#         wordlist = text.split()
-#         amount_miss = len(list(self.spellchecker.unknown(wordlist)))
-#
-#         return amount_miss
-#
-#     def add_spelling_dictionary(self, tokens: List[str]) -> List[str]:
-#         """dictionary update for pyspell checker and autocorrect"""
-#         self.spellchecker.word_frequency.load_words(tokens)
-#         self.speller.nlp_data.update({token: 1000 for token in tokens})
-#
-#     def run(
-#         self,
-#         prompts: pd.DataFrame,
-#         summaries: pd.DataFrame,
-#     ) -> pd.DataFrame:
-#
-#         # before merge preprocess
-#         prompts["prompt_tokens"] = prompts["prompt_text"].apply(
-#             lambda x: word_tokenize(x)
-#         )
-#         prompts["prompt_length"] = prompts["prompt_tokens"].apply(len)
-#         summaries["summary_tokens"] = summaries["text"].apply(
-#             lambda x: word_tokenize(x)
-#         )
-#         summaries["summary_length"] = summaries["summary_tokens"].apply(len)
-#
-#         # Add prompt tokens into spelling checker dictionary
-#         prompts["prompt_tokens"].apply(lambda x: self.add_spelling_dictionary(x))
-#
-#         #         from IPython.core.debugger import Pdb; Pdb().set_trace()
-#         # fix misspelling
-#         summaries["fixed_summary_text"] = summaries["text"].progress_apply(
-#             lambda x: self.speller(x)
-#         )
-#
-#         # count misspelling
-#         summaries["splling_err_num"] = summaries["text"].progress_apply(self.spelling)
-#
-#         # merge prompts and summaries
-#         input_df = summaries.merge(prompts, how="left", on="prompt_id")
-#
-#         # after merge preprocess
-#         input_df["length_ratio"] = (
-#             input_df["summary_length"] / input_df["prompt_length"]
-#         )
-#
-#         input_df["word_overlap_count"] = input_df.progress_apply(
-#             self.word_overlap_count, axis=1
-#         )
-#         input_df["bigram_overlap_count"] = input_df.progress_apply(
-#             self.ngram_co_occurrence, args=(2,), axis=1
-#         )
-#         input_df["bigram_overlap_ratio"] = input_df["bigram_overlap_count"] / (
-#             input_df["summary_length"] - 1
-#         )
-#
-#         input_df["trigram_overlap_count"] = input_df.progress_apply(
-#             self.ngram_co_occurrence, args=(3,), axis=1
-#         )
-#         input_df["trigram_overlap_ratio"] = input_df["trigram_overlap_count"] / (
-#             input_df["summary_length"] - 2
-#         )
-#
-#         input_df["quotes_count"] = input_df.progress_apply(self.quotes_count, axis=1)
-#
-#         input_df["fold"] = input_df["prompt_id"].map(ID2FOLD)
-#
-#         return input_df.drop(columns=["summary_tokens", "prompt_tokens"])
-#
-#
-# def get_preprocessed_dataset(prompts, summaries, save_path, preprocessor=None):
-#     save_path = Path(save_path)
-#     save_path.parent.mkdir(parents=True, exist_ok=True)
-#     if save_path.exists():
-#         return torch.load(save_path)
-#
-#     if preprocessor is None:
-#         preprocessor = Preprocessor()
-#     input_df = preprocessor.run(
-#         prompts=prompts,
-#         summaries=summaries,
-#     )
-#     torch.save(input_df, save_path)
-#
-#     return input_df
-#
-#
-# def choose_fold(row, target, ignore_fold):
-#     if ignore_fold:
-#         clean_targets = [f"{target}_{idx}" for idx in range(4)]
-#     else:
-#         fold = row["fold"]
-#         clean_targets = [f"{target}_{fold}"]
-#     mean_value = row[clean_targets].mean()
-#     return mean_value
-#
-#
-# def merge_features(preprocessed_df, deberta_features, ignore_fold=False):
-#     # mean of non-leak predictions
-#     merged_df = pd.merge(preprocessed_df, deberta_features, on="student_id", how="left")
-#     merged_df["deberta_content"] = merged_df.apply(lambda x: choose_fold(x, "content", ignore_fold), axis=1)
-#     merged_df["deberta_wording"] = merged_df.apply(lambda x: choose_fold(x, "wording", ignore_fold), axis=1)
-#
-#     merged_df = merged_df.drop(columns=[f"content_{f}" for f in range(4)] + [f"wording_{f}" for f in range(4)])
-#
-#     return merged_df
-#
-#
-# def train_lgbm(train, targets, drop_columns):
-#     # https://colab.research.google.com/drive/181GCGp36_75C2zm7WLxr9U2QjMXXoibt#scrollTo=aIhxl7glaJ5k
-#     # defaults
-#     default_params = {
-#         'boosting_type': 'gbdt',
-#         'random_state': 42,
-#         'objective': 'regression',
-#         'metric': 'rmse',
-#         'learning_rate': 0.05,
-#         'boosting': "gbdt",
-#         'num_leaves': 31,
-#         'max_depth': -1,
-#         'min_data_in_leaf': 20,
-#         'min_data_in_bin': 3,
-#         'lambda_l1': 0.0,
-#         'lambda_l2': 0.0,
-#     }
-#     # Initialize a new wandb run
-#     wandb.init(
-#         config=default_params,
-#         reinit=True,
-#         project=WandbProjects.WANDB_LGBM_FOLDS,
-#         tags=get_wandb_tags(),
-#     )
-#
-#     TRAINING_COLUMNS = None
-#     model_dict = {}
-#
-#     for target in targets:
-#         models = []
-#
-#         for fold in range(4):
-#             print(f"target={target}, fold={fold}")
-#
-#             X_train_cv = train[train["fold"] != fold].drop(columns=drop_columns)
-#             y_train_cv = train[train["fold"] != fold][target]
-#             TRAINING_COLUMNS = X_train_cv.columns  # to check with prediction
-#
-#             X_eval_cv = train[train["fold"] == fold].drop(columns=drop_columns)
-#             y_eval_cv = train[train["fold"] == fold][target]
-#
-#             dtrain = lgb.Dataset(X_train_cv, label=y_train_cv)
-#             dval = lgb.Dataset(X_eval_cv, label=y_eval_cv)
-#
-#             evaluation_results = {}
-#             # deepcopy may cause MaxRecursionException
-#             hyperparameters = {k:v for k, v in wandb.config.items()}
-#
-#             model = lgb.train(hyperparameters,
-#                               num_boost_round=10000,
-#                               # categorical_feature = categorical_features,
-#                               valid_names=['train_single_fold', 'valid'],
-#                               train_set=dtrain,
-#                               valid_sets=dval,
-#                               callbacks=[
-#                                   lgb.early_stopping(stopping_rounds=30, verbose=True),
-#                                   lgb.log_evaluation(100),
-#                                   lgb.callback.record_evaluation(evaluation_results),
-#                                   # wandb_callback(),
-#                               ],
-#                               )
-#             # log_summary(model, save_model_checkpoint=True)
-#             models.append(model)
-#
-#         model_dict[target] = models
-#
-#     # cv
-#     metrics = {}
-#
-#     for target in targets:
-#         models = model_dict[target]
-#
-#         preds = []
-#         trues = []
-#
-#         for fold, model in enumerate(models):
-#             X_eval_cv = train[train["fold"] == fold].drop(columns=drop_columns)
-#             assert (X_eval_cv.columns == TRAINING_COLUMNS).all()
-#             y_eval_cv = train[train["fold"] == fold][target]
-#
-#             pred = model.predict_single_fold(X_eval_cv)
-#
-#             trues.extend(y_eval_cv)
-#             preds.extend(pred)
-#
-#         rmse = np.sqrt(mean_squared_error(trues, preds))
-#         metrics[target] = rmse
-#
-#     mcrmse = sum(metrics.values()) / len(metrics)
-#     metrics["mcrmse"] = mcrmse
-#     wandb.log(metrics)
-#
-#     return model_dict, metrics
+class Preprocessor:
+    def __init__(self) -> None:
+        self.twd = TreebankWordDetokenizer()
+        self.STOP_WORDS = set(stopwords.words("english"))
+
+        self.speller = Speller(lang="en")
+        self.spellchecker = SpellChecker()
+
+    def word_overlap_count(self, row):
+        """intersection(prompt_text, text)"""
+
+        def check_is_stop_word(word):
+            return word in self.STOP_WORDS
+
+        prompt_words = row["prompt_tokens"]
+        summary_words = row["summary_tokens"]
+        if self.STOP_WORDS:
+            prompt_words = list(filter(check_is_stop_word, prompt_words))
+            summary_words = list(filter(check_is_stop_word, summary_words))
+        return len(set(prompt_words).intersection(set(summary_words)))
+
+    def ngrams(self, token, n):
+        # Use the zip function to help us generate n-grams
+        # Concatentate the tokens into ngrams and return
+        ngrams = zip(*[token[i:] for i in range(n)])
+        return [" ".join(ngram) for ngram in ngrams]
+
+    def ngram_co_occurrence(self, row, n: int) -> int:
+        # Tokenize the original text and summary into words
+        original_tokens = row["prompt_tokens"]
+        summary_tokens = row["summary_tokens"]
+
+        # Generate n-grams for the original text and summary
+        original_ngrams = set(self.ngrams(original_tokens, n))
+        summary_ngrams = set(self.ngrams(summary_tokens, n))
+
+        # Calculate the number of common n-grams
+        common_ngrams = original_ngrams.intersection(summary_ngrams)
+        return len(common_ngrams)
+
+    def quotes_count(self, row):
+        summary = row["text"]
+        text = row["prompt_text"]
+        quotes_from_summary = re.findall(r'"([^"]*)"', summary)
+        if len(quotes_from_summary) > 0:
+            return [quote in text for quote in quotes_from_summary].count(True)
+        else:
+            return 0
+
+    def spelling(self, text):
+
+        wordlist = text.split()
+        amount_miss = len(list(self.spellchecker.unknown(wordlist)))
+
+        return amount_miss
+
+    def add_spelling_dictionary(self, tokens: List[str]) -> List[str]:
+        """dictionary update for pyspell checker and autocorrect"""
+        self.spellchecker.word_frequency.load_words(tokens)
+        self.speller.nlp_data.update({token: 1000 for token in tokens})
+
+    def run(
+        self,
+        prompts: pd.DataFrame,
+        summaries: pd.DataFrame,
+    ) -> pd.DataFrame:
+
+        # before merge preprocess
+        prompts["prompt_tokens"] = prompts["prompt_text"].apply(
+            lambda x: word_tokenize(x)
+        )
+        prompts["prompt_length"] = prompts["prompt_tokens"].apply(len)
+        summaries["summary_tokens"] = summaries["text"].apply(
+            lambda x: word_tokenize(x)
+        )
+        summaries["summary_length"] = summaries["summary_tokens"].apply(len)
+
+        # Add prompt tokens into spelling checker dictionary
+        prompts["prompt_tokens"].apply(lambda x: self.add_spelling_dictionary(x))
+
+        #         from IPython.core.debugger import Pdb; Pdb().set_trace()
+        # fix misspelling
+        summaries["fixed_summary_text"] = summaries["text"].progress_apply(
+            lambda x: self.speller(x)
+        )
+
+        # count misspelling
+        summaries["splling_err_num"] = summaries["text"].progress_apply(self.spelling)
+
+        # merge prompts and summaries
+        input_df = summaries.merge(prompts, how="left", on="prompt_id")
+
+        # after merge preprocess
+        input_df["length_ratio"] = (
+            input_df["summary_length"] / input_df["prompt_length"]
+        )
+
+        input_df["word_overlap_count"] = input_df.progress_apply(
+            self.word_overlap_count, axis=1
+        )
+        input_df["bigram_overlap_count"] = input_df.progress_apply(
+            self.ngram_co_occurrence, args=(2,), axis=1
+        )
+        input_df["bigram_overlap_ratio"] = input_df["bigram_overlap_count"] / (
+            input_df["summary_length"] - 1
+        )
+
+        input_df["trigram_overlap_count"] = input_df.progress_apply(
+            self.ngram_co_occurrence, args=(3,), axis=1
+        )
+        input_df["trigram_overlap_ratio"] = input_df["trigram_overlap_count"] / (
+            input_df["summary_length"] - 2
+        )
+
+        input_df["quotes_count"] = input_df.progress_apply(self.quotes_count, axis=1)
+
+        input_df["fold"] = input_df["prompt_id"].map(ID2FOLD)
+
+        return input_df.drop(columns=["summary_tokens", "prompt_tokens"])
+
+
+def get_preprocessed_dataset(prompts, summaries, save_path, preprocessor=None):
+    save_path = Path(save_path)
+    save_path.parent.mkdir(parents=True, exist_ok=True)
+    if save_path.exists():
+        return torch.load(save_path)
+
+    if preprocessor is None:
+        preprocessor = Preprocessor()
+    input_df = preprocessor.run(
+        prompts=prompts,
+        summaries=summaries,
+    )
+    torch.save(input_df, save_path)
+
+    return input_df
+
+
+def train_lgbm(train, targets, drop_columns):
+    # https://colab.research.google.com/drive/181GCGp36_75C2zm7WLxr9U2QjMXXoibt#scrollTo=aIhxl7glaJ5k
+    # defaults
+    default_params = {
+        'random_state': 42,
+        'objective': 'regression',
+        'metric': 'rmse',
+        'learning_rate': 0.05,
+        'boosting': "gbdt",
+        'num_leaves': 31,
+        'max_depth': -1,
+        'min_data_in_leaf': 20,
+        'min_data_in_bin': 3,
+        'lambda_l1': 0.0,
+        'lambda_l2': 0.0,
+    }
+    # Initialize a new wandb run
+    wandb.init(
+        config=default_params,
+        reinit=True,
+        project=WandbProjects.WANDB_LGBM_FOLDS,
+        tags=get_wandb_tags(),
+    )
+
+    TRAINING_COLUMNS = None
+    model_dict = {}
+
+    for target in targets:
+        models = []
+
+        for fold in range(4):
+            print(f"target={target}, fold={fold}")
+
+            X_train_cv = train[train["fold"] != fold].drop(columns=drop_columns)
+            y_train_cv = train[train["fold"] != fold][target]
+            TRAINING_COLUMNS = X_train_cv.columns  # to check with prediction
+
+            X_eval_cv = train[train["fold"] == fold].drop(columns=drop_columns)
+            y_eval_cv = train[train["fold"] == fold][target]
+
+            dtrain = lgb.Dataset(X_train_cv, label=y_train_cv)
+            dval = lgb.Dataset(X_eval_cv, label=y_eval_cv)
+
+            evaluation_results = {}
+            # deepcopy may cause MaxRecursionException
+            hyperparameters = {k:v for k, v in wandb.config.items()}
+
+            model = lgb.train(hyperparameters,
+                              num_boost_round=10000,
+                              # categorical_feature = categorical_features,
+                              valid_names=['train_single_fold', 'valid'],
+                              train_set=dtrain,
+                              valid_sets=dval,
+                              callbacks=[
+                                  lgb.early_stopping(stopping_rounds=30, verbose=True),
+                                  lgb.log_evaluation(100),
+                                  lgb.callback.record_evaluation(evaluation_results),
+                                  # wandb_callback(),
+                              ],
+                              )
+            # log_summary(model, save_model_checkpoint=True)
+            models.append(model)
+
+        model_dict[target] = models
+
+    # cv
+    metrics = {}
+
+    for target in targets:
+        models = model_dict[target]
+
+        preds = []
+        trues = []
+
+        for fold, model in enumerate(models):
+            X_eval_cv = train[train["fold"] == fold].drop(columns=drop_columns)
+            assert (X_eval_cv.columns == TRAINING_COLUMNS).all()
+            y_eval_cv = train[train["fold"] == fold][target]
+
+            pred = model.predict(X_eval_cv)
+
+            trues.extend(y_eval_cv)
+            preds.extend(pred)
+
+        rmse = np.sqrt(mean_squared_error(trues, preds))
+        metrics[target] = rmse
+
+    mcrmse = sum(metrics.values()) / len(metrics)
+    metrics["mcrmse"] = mcrmse
+    wandb.log(metrics)
+
+    return model_dict, metrics
