53b204bf936685a3ecb39fc7a3e86f707a4ee9e9
Lukasz Cesarski <l.cesarski@edrone.me>
2023-09-30 18:51:36 +0200

    Add FBP data

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,8 @@
 mkdir -p input/commonlit-evaluate-student-summaries
-
 kaggle competitions download -c commonlit-evaluate-student-summaries -p input/
-
 unzip input/commonlit-evaluate-student-summaries.zip -d input/commonlit-evaluate-student-summaries
+
+
+mkdir -p input/feedback-prize-english-language-learning
+kaggle competitions download -c feedback-prize-english-language-learning -p input/
+unzip input/feedback-prize-english-language-learning.zip -d input/feedback-prize-english-language-learning
