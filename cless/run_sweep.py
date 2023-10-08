15eb60e7c004227ce94bf4938c86d92e5bebd1bf
Lukasz Cesarski <l.cesarski@edrone.me>
2023-10-08 12:51:02 +0200

    adjust learning rate for xlarge

--- cless/run_sweep.py
+++ cless/run_sweep.py
@@ -102,8 +102,8 @@ SWEEP_CONFIG_XLARGE = {
         },
         "learning_rate": {
             "distribution": "log_uniform_values",
-            "min": 1e-7,
-            "max": 5e-5,
+            "min": 1e-6,
+            "max": 1e-4,
         },
         "hidden_dropout_prob": {
             "distribution": "log_uniform_values",
